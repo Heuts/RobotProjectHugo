078a26a6c7a486b459fb2eac5af92e2302b6dbac
Heuts <hugo.heuts@hva.nl>
2018-10-17 12:31:08 +0200

    RobotLauncherV1.0

--- C13-B/src/main/RobotLauncher.java
+++ C13-B/src/main/RobotLauncher.java
@@ -10,36 +10,83 @@ import lejos.utility.Delay;
 public class RobotLauncher {
 
 	Brick brick;
-	
+
 	public RobotLauncher() {
 		super();
 		brick = LocalEV3.get();
-		welcomeMessage();
+		// welcomeMessage();
+		choiceMenu();
 	}
-	
-	//TODO: Gebruiker begroeten --> is waitforkey nodig?
+
+	// TODO: Gebruiker begroeten --> is waitforkey nodig?
 	private void welcomeMessage() {
 		TextLCD display = brick.getTextLCD();
 		display.drawString("Welkom", 0, 3);
-		display.drawString("Team!", 0, 4);
-		Delay.msDelay(3000); //aantal seconden dat bericht zichtbaar is
+		display.drawString("Team Bravo!", 0, 4);
+		Delay.msDelay(3000); // aantal miliseconden dat bericht zichtbaar is
 	}
-	
-	//TODO: methode(s) die verschillende applicaties oplijst en user vraagt welk programma 
-	//gerunned moet worden en roept methode aan
-	
-	//TODO: methode om app1 te starten
-	//TODO: methode om app2 te starten
-	
-	
+
+	// TODO: methode(s) die verschillende applicaties oplijst en user vraagt welk
+	// programma gerunned moet worden en roept methode aan
+	private void choiceMenu() {
+		TextLCD display = brick.getTextLCD();
+		display.drawString("Welkom team Bravo!", 0, 0);
+		display.drawString("Welke app?", 0, 2);
+		display.drawString("App 1 (UP)", 0, 3);
+		display.drawString("App 2 (DOWN)", 0, 4);
+		display.drawString("App 3 (RIGHT)", 0, 5);
+		display.drawString("Exit (ESCAPE)", 0, 7);
+//		Delay.msDelay(3000);
+//		waitForKey(Button.UP);
+//		waitForKey(Button.DOWN);
+//		display.clear();
+//		display.refresh();
+		Button.waitForAnyEvent();
+//		while (Button.ESCAPE.isUp()) {
+		if (Button.UP.isDown()) {
+			display.clear();
+			display.refresh();
+			display.drawString("App 1", 0, 1);
+			Delay.msDelay(500);
+		}
+		if (Button.DOWN.isDown()) {
+			display.clear();
+			display.drawString("App 2", 0, 1);
+			Delay.msDelay(500);
+		}
+		if (Button.RIGHT.isDown()) {
+			display.clear();
+			display.drawString("App 3", 0, 1);
+			Delay.msDelay(500);
+		}
+		if (Button.ESCAPE.isDown()) {
+			display.clear();
+			display.drawString("EXIT", 0, 1);
+			Delay.msDelay(500);
+		}
+	}
+
+//		Delay.msDelay(10000);
+//	}
+
+	// TODO: methode om app1 te starten
+	private void startApp1() {
+		// new BlackLine();
+	}
+
+	// TODO: methode om app2 te starten
+	private void startApp2() {
+		// new object keuzeproject
+	}
+
 	/*
 	 * Deze methode is nodig bij het wachten tot user app heeft gekozen
 	 */
 	public void waitForKey(Key key) {
-		while(key.isUp()) {
+		while (key.isUp()) {
 			Delay.msDelay(100);
 		}
-		while(key.isDown()) {
+		while (key.isDown()) {
 			Delay.msDelay(100);
 		}
 	}
