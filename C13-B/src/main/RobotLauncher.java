7039e31f05b96c49ec929779a90f08d85a23f3a9
tom <tom.vanbesien@gmail.com>
2018-10-31 14:25:34 +0100

    robot launcher loop toegevoegd zodat alle apps na elkaar kunnen gekozen worden zonder reboot programma

--- C13-B/src/main/RobotLauncher.java
+++ C13-B/src/main/RobotLauncher.java
@@ -10,9 +10,11 @@ import utility.Lcd;
 
 public class RobotLauncher {
 
-	final static String APP1 = "Line Follower";
-	final static String APP2 = "Find Beacon";
-	final static String APP3 = "Play Tag";
+	private final static String APP1 = "Line Follower";
+	private final static String APP2 = "Find Beacon";
+	private final static String APP3 = "Play Tag";
+	
+	private static boolean running;
 
 	/*
 	 * Assigning ports
@@ -20,19 +22,19 @@ public class RobotLauncher {
 	private final static Port	LEFT_COLOR = SensorPort.S1,
 								RIGHT_COLOR = SensorPort.S2,
 								PRESSURE_FRONT = SensorPort.S3,
-								PRESSURE_BACK = SensorPort.S2,
+								PRESSURE_BACK = SensorPort.S1,
 								INFRARED = SensorPort.S4,
 								LEFT_MOTOR = MotorPort.A,
 								MIDDLE_MOTOR = MotorPort.C,
 								RIGHT_MOTOR = MotorPort.D;
 								
-							
-							  
-
 	public RobotLauncher() {
-		super();
+		running = true;
 		printWelcome();
-		printMenu();
+		
+		while(running) {
+			printMenu();
+		}
 	}
 
 	private void printWelcome() {
@@ -51,45 +53,44 @@ public class RobotLauncher {
 		Lcd.print(5, APP3+" (RIGHT)");
 		Lcd.print(7, "Exit (ESCAPE)");
 		
-		Button.waitForAnyEvent();
-		if (Button.UP.isDown()) {
-			
-			Lcd.clear();
-			Lcd.print(1, APP1);
-			startApp1();
-			
-			Delay.msDelay(500);
-			
-		}
-		if (Button.DOWN.isDown()) {
-			
-			Lcd.clear();
-			Lcd.print(1, APP2);
-			startApp2();
-			
-			Delay.msDelay(500);
-		}
-		if (Button.RIGHT.isDown()) {
-			
-			Lcd.clear();
-			Lcd.print(1, APP3);
-			startApp3();
-			
-			Delay.msDelay(500);
-		}
-		if (Button.ESCAPE.isDown()) {
-			Lcd.clear();
-			Lcd.print(1, "EXIT");
-			Delay.msDelay(500);
+		boolean choiceMade = false;
+		
+		while(!choiceMade) {
+			if (Button.UP.isDown()) {
+				choiceMade = true;
+				Lcd.clear();
+				Lcd.print(1, APP1);
+				startApp1();
+				Delay.msDelay(500);
+			}
+			if (Button.DOWN.isDown()) {
+				choiceMade = true;
+				Lcd.clear();
+				Lcd.print(1, APP2);
+				startApp2();
+				Delay.msDelay(500);
+			}
+			if (Button.RIGHT.isDown()) {
+				choiceMade = true;
+				Lcd.clear();
+				Lcd.print(1, APP3);
+				startApp3();
+				Delay.msDelay(500);
+			}
+			if (Button.ESCAPE.isDown()) {
+				choiceMade = true;
+				Lcd.clear();
+				Lcd.print(1, "EXIT");
+				running = false;
+				Delay.msDelay(500);
+			}
 		}
 	}
 
-	// TODO: methode om app1 te starten
 	private void startApp1() {
 		new model.linefollower.Launcher(LEFT_MOTOR, RIGHT_MOTOR, LEFT_COLOR, RIGHT_COLOR);
 	}
 
-	// TODO: methode om app2 te starten
 	private void startApp2() {
 		new model.beaconfinder.Launcher(LEFT_MOTOR, RIGHT_MOTOR, MIDDLE_MOTOR, INFRARED, PRESSURE_FRONT);
 	}
