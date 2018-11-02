66083baea814a39f1e5540cd34b0504c660ed0ae
JKSS <39271637+Hodori88@users.noreply.github.com>
2018-11-02 09:51:00 +0100

    Cannon voorzien van comments

--- C13-B/src/model/beaconfinder/Cannon.java
+++ C13-B/src/model/beaconfinder/Cannon.java
@@ -2,6 +2,9 @@ package model.beaconfinder;
 
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 
+/* Secundaire motor voor kanon functie heeft 3 rotaties van 360 graden nodig om het volgende projectiel te 'laden' en op 'scherp te zetten'
+ */
+
 public class Cannon {
 	
 	EV3MediumRegulatedMotor cannon;
@@ -10,17 +13,23 @@ public class Cannon {
         this.cannon = cannon;
 	}
     
-    public void cannonFire() {
+    void cannonFire() {
         fire();
 		resetMotor();
     }
-    
+
+    /*
+     * Om de motor te resetten voor het volgend projectiel, draait de motor de resterende 2,5 rotaties door.
+     */
     private void resetMotor(){
         cannon.rotate(890);
         cannon.resetTachoCount();
     }       
 
-
+    /*
+     *  Met iets meer dan een 0,5 rotatie (190) zal het projectiel worden afgevuurd. 
+     */
+    *
     private void fire(){
         cannon.setSpeed(1000);
         cannon.setAcceleration(6000);
